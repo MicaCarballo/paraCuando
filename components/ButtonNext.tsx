1b0df4b9d1b33bbd27a4746e1251a609cae0645b
MicaCarballo <micacarballo2@gmail.com>
2023-01-17 11:35:09 -0300

    feat: creacion de atomos

---
+++ components/ButtonNext.tsx
@@ -0,0 +1,11 @@
+const ButtonNext = () => {
+  return (
+    <div className="button-next">
+      <button>
+        <div className="icon"></div>
+      </button>
+    </div>
+  );
+};
+
+export default ButtonNext;
