c5e62a7e4fda40fcd9b6a36478996546747efdda
micaela <micaela@MacBook-Air-de-micaela.local>
2023-01-26 14:20:46 -0300

    fix : like button responsive

--- components/LikedIcon.tsx
+++ components/LikedIcon.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import likeIcon from '../public/likedIcon.png';
 const LikedIcon = () => {
+  const [liked, setliked] = useState(false);
+
   return (
-    <div className="liked-icon">
-      <button>
-        <div className="icon"></div>
-      </button>
-    </div>
+    <button
+      className={` flex items-center justify-center p-2 h-12 w-12 border-white rounded-full ${
+        liked ? ' bg-primarypink' : ' bg-primarygrayLight'
+      }`}
+      onClick={() => setliked(!liked)}
+    >
+      <Image src={likeIcon} alt="like" />
+    </button>
   );
 };
 
