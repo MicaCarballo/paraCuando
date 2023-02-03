cc18f96942d94cca5b23ae56c18592fb7b896bbe
hilcias <hilcias_7@hotmail.com>
2023-02-03 16:02:17 -0500

    feat: tarea Agregar espaciado y container para elementos en profile page

--- pages/config.tsx
+++ pages/config.tsx
@@ -9,57 +9,75 @@ const Config = () => {
       description="Perfil de Para Cuando?"
     >
       <div className="w-full h-32 bg-primaryblue relative">
-        <div className="text-6xl translate-x-56 font-black text-[#F8F7FA]">
+        <div className="text-6xl absolute translate-x-56 font-black text-[#F8F7FA] top-[25px]">
           Perfil
         </div>
       </div>
-      <div className="relative grid grid-flow-col">
-        <div className="w-full h-32 bg-white">
-          <h2 className="text-center relative text-2xl font-medium top-20">
-            Foto de perfil
-          </h2>
-          <span className="relative text-center left-32 top-20">
-            Agrega una foto para tu perfil
-          </span>
-          <Image
-            src={Rectangle183}
-            width={177}
-            height={206}
-            alt=""
-            className="relative left-1/2  -translate-x-1/2 top-28"
-          ></Image>
 
-          <Image
-            src={Vectormas}
-            width={17}
-            height={17}
-            alt=""
-            className="relative left-1/2  -translate-x-1/2 "
-          ></Image>
+      <div className="flex flex-wrap justify-center gap-5 mt-10 md:gap-20 md:m-20">
+        <div>
+          <form action="" className=" mt-0 flex flex-row gap-36">
+            <div className="flex flex-col items-start w-36">
+              <h3 className="text-center text-2xl font-medium ">
+                Foto de perfil
+              </h3>
+
+              <span className="h400-normal-16px text-primary-blackLight w-[208px]">
+                Agrega una foto para tu perfil
+              </span>
+              <div className="relative mt-8">
+                <label htmlFor="file-input" className="img1">
+                  <Image
+                    src={Rectangle183}
+                    width={177}
+                    height={206}
+                    alt=""
+                    className="relative justify-center"
+                  ></Image>
+
+                  <Image
+                    src={Vectormas}
+                    width={17}
+                    height={17}
+                    alt=" "
+                    className="absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 "
+                  ></Image>
+                </label>
+                <input
+                  id="file-input"
+                  type="file"
+                  accept="image/*"
+                  className="file:hidden file:apperence-none file:invisible file:opacity-0 hidden "
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-14">
+              <h2 className=" text-2xl font-medium ">
+                Información del Contacto
+              </h2>
+              <label htmlFor="firstName" className=" relative bg-transparent">
+                <input
+                  id="firstName"
+                  type="text"
+                  className="pl-3 w-3/4 md:w-[620px] h-14 border border-solid border-gray-700 rounded-md "
+                />
+                <span className=" bg-white absolute left-2.5 top-5 px-2  transform -translate-y-7 -translate-4 text-[gray]">
+                  First Name
+                </span>
+              </label>
+              <label htmlFor="lastName" className=" relative">
+                <input
+                  id="lastName"
+                  type="text"
+                  className="pl-3 w-3/4 md:w-[620px] h-14 border border-solid border-gray-700 rounded-md "
+                />
+                <span className=" bg-white absolute left-2.5 top-5 px-2  transform -translate-y-7 -translate-4 text-[gray]">
+                  Last Name
+                </span>
+              </label>
+            </div>
+          </form>
         </div>
-        <form action="" className=" mt-11 flex flex-col gap-4">
-          <h2 className="m-2 text-2xl font-medium ">
-            Información del Contacto
-          </h2>
-          <label htmlFor="" className=" relative">
-            <input
-              type="text"
-              className="w-3/4 h-11 border border-solid border-gray-700 rounded-md "
-            />
-            <span className=" bg-white absolute left-2.5 top-5 px-2  transform -translate-y-7 -translate-4 text-[gray]">
-              First Name
-            </span>
-          </label>
-          <label htmlFor="" className=" relative">
-            <input
-              type="text"
-              className="w-3/4 h-11 border border-solid border-gray-700 rounded-md "
-            />
-            <span className=" bg-white absolute left-2.5 top-5 px-2  transform -translate-y-7 -translate-4 text-[gray]">
-              Last Name
-            </span>
-          </label>
-        </form>
       </div>
     </Layout>
   );
